0dbe9bae912d701e0f9dee1b7595b70cf7848eef
Luxibol <mathieu.billoux@outlook.fr>
2025-03-17 10:55:59 +0100

    Initialize frontend structure with routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="decorative-line">Vite + React</h1>
-      <div className="card">
-        <button className="btn-primary" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Page d'accueil (à venir)</div>} />
+        <Route path="/entreprises/:categoryId" element={<div>Liste des entreprises (à venir)</div>} />
+        <Route path="/entreprise/:id" element={<div>Détails de l'entreprise (à venir)</div>} />
+      </Routes>
+    </div>
   );
 }
 
