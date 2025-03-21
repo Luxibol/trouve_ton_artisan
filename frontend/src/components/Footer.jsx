5bc4cd058a167f638734b28f308a1e6d6b5b1a5a
Luxibol <mathieu.billoux@outlook.fr>
2025-03-21 22:03:40 +0100

    SCSS footer clenup for future simplification

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,21 +3,22 @@ import logo from '../assets/pictures/Logo.png';
 
 function Footer() {
   return (
-    <footer className="footer-bg-color text-white py-4 mt-5">
+    <footer className="bg-primary text-white py-4 mt-5">
       <div className="container">
         <div className="row">
-          <div className="logo-container col-md-6">
+          <div className="col-md-6 mb-3 mb-md-0">
             <Link to="/">
               <img
-               src={logo}
-               alt="Trouve Ton Artisan Logo"
-               style={{ height: '150px' }}
+                src={logo}
+                alt="Trouve Ton Artisan Logo"
+                className="img-fluid"
+                style={{ height: '150px' }}
               />
             </Link>
           </div>
           <div className="col-md-6">
-            <p>Lyon</p>
-            <p>
+            <p className="mb-1">Lyon</p>
+            <p className="mb-1">
               101 cours Charlemagne<br />
               CS 20033<br />
               69269 LYON CEDEX 02<br />
@@ -25,17 +26,14 @@ function Footer() {
               <a href="tel:+33426734000" className="text-white">+33 (0)4 26 73 40 00</a>
             </p>
           </div>
-
-        <hr className="bg-light" />
-        <div>
-            <ul className="list-unstyled d-flex gap-3 justify-content-md-center">
-              <li><Link to="/legal/mentions" className="text-white text-decoration-none">Mentions légales</Link></li>
-              <li><Link to="/legal/donnees-personnelles" className="text-white text-decoration-none">Données personnelles</Link></li>
-              <li><Link to="/legal/accessibilite" className="text-white text-decoration-none">Accessibilité</Link></li>
-              <li><Link to="/legal/cookies" className="text-white text-decoration-none">Cookies</Link></li>
-            </ul>
         </div>
-        </div>
+        <hr className="bg-light my-3" />
+        <ul className="list-unstyled d-flex flex-column flex-md-row justify-content-md-center gap-3">
+          <li><Link to="/legal/mentions" className="text-white text-decoration-none">Mentions légales</Link></li>
+          <li><Link to="/legal/donnees-personnelles" className="text-white text-decoration-none">Données personnelles</Link></li>
+          <li><Link to="/legal/accessibilite" className="text-white text-decoration-none">Accessibilité</Link></li>
+          <li><Link to="/legal/cookies" className="text-white text-decoration-none">Cookies</Link></li>
+        </ul>
       </div>
     </footer>
   );
