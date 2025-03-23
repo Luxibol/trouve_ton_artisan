57e59ac756f944bef7429c7778412e49ebb75ee2
Luxibol <mathieu.billoux@outlook.fr>
2025-03-23 16:50:08 +0100

    Formating the footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,25 +1,25 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/pictures/Logo.png';
 
+// Composant Footer pour afficher le pied de page du site
 function Footer() {
   return (
-    <footer className="bg-primary text-white py-4 mt-5">
+    <footer className="py-1 pt-md-5 mt-5">
       <div className="container">
         <div className="row">
-          <div className="col-md-6 mb-3 mb-md-0">
+          <div className="col-md-6 px-md-5">
             <Link to="/">
               <img
                 src={logo}
-                alt="Trouve Ton Artisan Logo"
-                className="img-fluid"
-                style={{ height: '150px' }}
+                alt="Logo Trouve Ton Artisan"
+                className="img-fluid px-md-5 logo-footer position-relative top-n5"
               />
             </Link>
           </div>
-          <div className="col-md-6">
-            <p className="mb-1">Lyon</p>
+          <div className="col-md-6 px-5 ">
+            <p className="mb-1 fw-bold">Lyon</p>
             <p className="mb-1">
-              101 cours Charlemagne<br />
+              101 cours Charlemagne<br/>
               CS 20033<br />
               69269 LYON CEDEX 02<br />
               France<br />
@@ -27,8 +27,8 @@ function Footer() {
             </p>
           </div>
         </div>
-        <hr className="bg-light my-3" />
-        <ul className="list-unstyled d-flex flex-column flex-md-row justify-content-md-center gap-3">
+        <hr className="bg-light opacity-100 border-white my-3"/>
+        <ul className="list-unstyled d-flex flex-column flex-md-row justify-content-md-center gap-md-4 gap-1 px-4"> 
           <li><Link to="/legal/mentions" className="text-white text-decoration-none">Mentions légales</Link></li>
           <li><Link to="/legal/donnees-personnelles" className="text-white text-decoration-none">Données personnelles</Link></li>
           <li><Link to="/legal/accessibilite" className="text-white text-decoration-none">Accessibilité</Link></li>
